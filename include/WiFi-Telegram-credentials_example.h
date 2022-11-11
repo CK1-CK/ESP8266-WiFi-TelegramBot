feeedec1a03fa6052d7ccd7d01762b95a0c363e8
CK1 <kreissc1@gmail.com>
2022-11-11 14:12:03 +0100

    Initial Commit

---
+++ include/WiFi-Telegram-credentials_example.h
@@ -0,0 +1,13 @@
+//Readme
+//Datei umbenennen _example entfernen
+//SSID und PW des WLAN einpflegen
+
+// Deine WLAN-Zugangsdaten
+const char* ssid = "NETZWERK";
+const char* password = "PASSWORT";
+
+// Den Telegram-Bot initialisieren
+#define botToken "TOKEN"  // den Bot-Token bekommst du vom Botfather)
+
+//Deine UserID
+#define userID "USERID"
