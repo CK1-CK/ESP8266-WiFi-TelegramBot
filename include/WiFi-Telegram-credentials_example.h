23797396f0c05170362331f6bf8995e00ba01021
CK1 <kreissc1@gmail.com>
2022-11-15 15:46:27 +0100

    Array for more than one Wifi

--- include/WiFi-Telegram-credentials_example.h
+++ include/WiFi-Telegram-credentials_example.h
@@ -2,12 +2,12 @@
 //Datei umbenennen _example entfernen
 //SSID und PW des WLAN einpflegen
 
-// Deine WLAN-Zugangsdaten
-const char* ssid = "NETZWERK";
-const char* password = "PASSWORT";
+// WiFi-Credentials
+String ssids[] = {"SSID1", "SSID2", "SSID3"};
+String passwords[] = {"PW1", "PW2", "PW3"};
 
 // Den Telegram-Bot initialisieren
 #define botToken "TOKEN"  // den Bot-Token bekommst du vom Botfather)
 
 //Deine UserID
-#define userID "12345678910"
+#define userID "123456789"
